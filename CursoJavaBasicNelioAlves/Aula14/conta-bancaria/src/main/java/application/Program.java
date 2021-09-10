0a87bab8810ba2d3aa08665c1712103c5419c42f
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-09-10 07:59:45 -0300

    Upcasting e downcasting

--- CursoJavaBasicNelioAlves/Aula14/conta-bancaria/src/main/java/application/Program.java
+++ CursoJavaBasicNelioAlves/Aula14/conta-bancaria/src/main/java/application/Program.java
@@ -1,11 +1,37 @@
 package application;
 
+import entities.Account;
+import entities.BusinessAccount;
+import entities.SavingAccount;
+
 public class Program {
 
 	public static void main(String[] args) {
-		BusinessAccount account = new BusinessAccount();
+		Account acc = new Account(1001, "Alex", 0.0);
+		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
-		account.getBalance();
+		// UPCASTING
+		// CONVERTER UM OBJETO DA SUPER CLASSE PARA A SUBCLASSE
+		Account acc1 = bacc;
+		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
+		Account acc3 = new SavingAccount(1004, "Anna", 0.0, 100.0);
+
+		// DOWNCASTING
+		// CONVERTER UM OBJETO DA SUBCLASSE PARA A SUPER CLASSE
+		BusinessAccount acc4 = (BusinessAccount) acc2;
+		acc4.loan(100.0);
+
+		if (acc3 instanceof BusinessAccount) {
+			BusinessAccount acc5 = (BusinessAccount) acc3;
+			acc5.loan(200.0);
+			System.out.println("Loan!");
+		}
+		
+		if (acc3 instanceof SavingAccount) {
+			SavingAccount acc5 = (SavingAccount) acc3;
+			acc5.updateBalance();
+			System.out.println("Update!");
+		}
 
 	}
 
