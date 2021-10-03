84567a2bcc1622b0aa04d65cc6f38a669a14831e
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-10-03 16:00:57 -0300

    organizing the projects

---
+++ UnitaryTests/junit4/mocks-and-asserts/mock-and-asserts/src/main/java/com/teste/CalculadoraNova.java
@@ -0,0 +1,13 @@
+package com.teste;
+
+public class CalculadoraNova {
+
+	public int somar(int... valores) {
+		int soma = 0;
+		for (int valor : valores) {
+			soma += valor;
+		}
+		return soma;
+	}
+
+}
