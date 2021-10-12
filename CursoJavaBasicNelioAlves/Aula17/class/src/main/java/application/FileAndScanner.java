e4f6186961eeb2ac3fbf43d5d3a5954d122589c7
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-10-12 10:25:38 -0300

    organizing class files

--- CursoJavaBasicNelioAlves/Aula17/class/src/main/java/application/FileAndScanner.java
+++ CursoJavaBasicNelioAlves/Aula17/class/src/main/java/application/FileAndScanner.java
@@ -4,7 +4,7 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
 
-public class Program {
+public class FileAndScanner {
 	
 	public static void main(String[] args) {
 		File file = new File("c:\\temp\\in.txt");
