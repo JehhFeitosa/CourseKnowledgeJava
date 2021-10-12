1259e6842f5b5b9b48e4b21319864ac9b08d324f
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-10-12 10:21:06 -0300

    class file and scanner

---
+++ CursoJavaBasicNelioAlves/Aula17/file-scanner/src/main/java/application/Program.java
@@ -0,0 +1,29 @@
+package application;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class Program {
+	
+	public static void main(String[] args) {
+		File file = new File("c:\\temp\\in.txt");
+		Scanner sc = null;
+		
+		try {
+			sc= new Scanner(file);
+			while(sc.hasNext()) {
+				System.out.println(sc.nextLine());
+			}
+		}
+		catch(IOException e) {
+			System.out.println("Error> " + e.getLocalizedMessage());
+		}
+		finally {
+			if(sc != null) { 
+			sc.close();
+			}
+		}
+	}
+
+}
