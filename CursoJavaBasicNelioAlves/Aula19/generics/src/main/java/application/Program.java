2e7ab1cc02310ad995c76372877f5ad94b0880d2
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-10-25 08:28:00 -0300

    generics

---
+++ CursoJavaBasicNelioAlves/Aula19/generics/src/main/java/application/Program.java
@@ -0,0 +1,31 @@
+package application;
+
+import java.util.Scanner;
+
+import services.PrintService;
+
+public class Program {
+
+	public static void main(String[] args) {
+		
+		Scanner sc = new Scanner(System.in);
+		
+		PrintService ps = new PrintService();
+		
+		System.out.println("How many values? ");
+		int n = sc.nextInt();
+		
+		for(int i = 0; i<n; i++) {
+			Integer value = sc.nextInt();
+			ps.addValue(value);
+		}
+		
+		
+		ps.print();
+		System.out.println("First: " + ps.first());
+		
+		sc.close();
+		
+	}
+
+}
