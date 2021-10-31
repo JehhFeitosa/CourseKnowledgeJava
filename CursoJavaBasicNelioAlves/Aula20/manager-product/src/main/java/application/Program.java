3d5805aea6f50ff082ecbaf1e172a2c6edcd2afc
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-10-31 11:16:09 -0300

    implements lambda

--- CursoJavaBasicNelioAlves/Aula20/manager-product/src/main/java/application/Program.java
+++ CursoJavaBasicNelioAlves/Aula20/manager-product/src/main/java/application/Program.java
@@ -1,7 +1,6 @@
 package application;
 
 import java.util.ArrayList;
-import java.util.Collections;
 import java.util.List;
 
 import entities.Product;
@@ -9,19 +8,19 @@ import entities.Product;
 public class Program {
 
 	public static void main(String[] args) {
-		
+
 		List<Product> list = new ArrayList<>();
-		
+
 		list.add(new Product("TV", 900.00));
 		list.add(new Product("Notebook", 1200.00));
 		list.add(new Product("Tablet", 450.00));
-		
-		Collections.sort(list);
-		
-		for(Product p: list) {
+
+		list.sort((p1, p2) -> p1.getName().toUpperCase().compareTo(p2.getName().toUpperCase()));
+
+		for (Product p : list) {
 			System.out.println(p);
 		}
-		
+
 	}
 
 }
--- CursoJavaBasicNelioAlves/Aula20/manager-product/src/main/java/entities/Product.java
+++ CursoJavaBasicNelioAlves/Aula20/manager-product/src/main/java/entities/Product.java
@@ -1,6 +1,6 @@
 package entities;
 
-public class Product implements Comparable<Product> {
+public class Product {
 	
 	private String name;
 	private Double price;
@@ -34,10 +34,7 @@ public class Product implements Comparable<Product> {
 		return "Product [name=" + name + ", price=" + price + "]";
 	}
 
-	public int compareTo(Product p) {
-		return name.toUpperCase().compareTo(p.getName().toUpperCase());
-	}
-
+	
 	
 	
 }
