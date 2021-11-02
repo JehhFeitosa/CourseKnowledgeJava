ebeb602ded702e7231001e601e89067925b50878
JehhFeitosa <jessicaadrianafeitosa@hotmail.com>
2021-11-02 17:47:46 -0300

    include data in database

--- CursoJavaBasicNelioAlves/Aula24/jpa-maven/src/main/java/aplicacao/Programa.java
+++ CursoJavaBasicNelioAlves/Aula24/jpa-maven/src/main/java/aplicacao/Programa.java
@@ -1,17 +1,37 @@
 package aplicacao;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 import dominio.Pessoa;
 
 public class Programa {
 
 	public static void main(String[] args) {
 
-		Pessoa p1 = new Pessoa(1, "Carlos da Silva", "carlos@gmail.com");
-		Pessoa p2 = new Pessoa(2, "Jessica da Silva", "jessica@gmail.com");
-		Pessoa p3 = new Pessoa(3, "Janailson da Silva", "janailson@gmail.com");
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
+		Pessoa p1 = new Pessoa(null, "Carlos da Silva", "carlos@gmail.com");
+		Pessoa p2 = new Pessoa(null, "Jessica da Silva", "jessica@gmail.com");
+		Pessoa p3 = new Pessoa(null, "Janailson da Silva", "janailson@gmail.com");
+		
+		//Conexão com o banco
+		EntityManagerFactory emf = Persistence.createEntityManagerFactory("exemplo-jpa");
+		
+		//Gerenciamento do banco
+		EntityManager em = emf.createEntityManager();
+		
+		//Inicia transação no banco de dados
+		em.getTransaction().begin();
+		
+		//Pega o objeto e salva no banco
+		em.persist(p1);
+		em.persist(p2);
+		em.persist(p3);
+		
+		//Confirma as transações que eu fiz
+		em.getTransaction().commit();
+				
+		System.out.println("Pronto!");
 	}
 
 }
--- CursoJavaBasicNelioAlves/Aula24/jpa-maven/src/main/java/dominio/Pessoa.java
+++ CursoJavaBasicNelioAlves/Aula24/jpa-maven/src/main/java/dominio/Pessoa.java
@@ -2,10 +2,22 @@ package dominio;
 
 import java.io.Serializable;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Pessoa implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	//Define chave primária do BD
+	@Id
+	//Gera a numeração do id sequencialmente
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
 	private Integer id;
+	
 	private String nome;
 	private String email;
 	
